35b789316545aa85cf295575d8b57175212b0097
fasilfaz <fazsby2001@gmail.com>
2023-12-05 15:26:53 +0500

    login and signup  added

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const buttons = document.querySelector('.buttons');
 const navList = document.querySelector('.navbar ul.menu-list');
+const forms = document.querySelector(".forms");
+
 
 buttons.addEventListener('click', () => {
     buttons.classList.toggle('active');
     navList.classList.toggle('active');
 });
+
+
+
+pwShowHide = document.querySelectorAll(".eye-icon"),
+links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+})
